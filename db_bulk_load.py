aabb2213210a80da51166d6bc50bafb04dbcde71
Jonas Andersson <Jonas123@gmail.com>
2014-03-11 16:53:09 -0500

    Created function for bulk loading nurse table

--- db_bulk_load.py
+++ db_bulk_load.py
@@ -1,7 +1,11 @@
 from faker import Factory
 import psycopg2
 gender = ['Male','Female']
-ethnicity = ['White','Black','American Indian','Asian Indian','Chinese','Filipino','Other Asian','Japanese','Korean','Vietnamese','Native Hawaiian','Guamanian','Samoan','Other Pacific Islander','Other']
+ethnicity = ['White','Black','American Indian',
+			 'Asian Indian','Chinese','Filipino',
+			 'Other Asian','Japanese','Korean',
+			 'Vietnamese','Native Hawaiian','Guamanian',
+			 'Samoan','Other Pacific Islander','Other']
 fake = Factory.create()
 
 # Connect to an existing database
@@ -11,27 +15,49 @@ conn = psycopg2.connect("dbname=postgres user=postgres password=hi host=localhos
 cur = conn.cursor()
 
 num_records = 1000
-insertions = 0
 id_photo = open("test_img.jpg","rb").read()
 
-while insertions < num_records:
-	name = fake.name()
-	village = fake.city()
-	dob = fake.date(pattern="%Y-%m-%d")
-	gend = fake.random_element(gender)
-	ethn = fake.random_element(ethnicity)
-	fp_hash = fake.md5(raw_output=False)
-	cur.execute("INSERT INTO clinic.patients(name, village, dob, gender, ethnicity, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
-											    (name,
-											     village, 
-											     dob,
-											     gend,
-											     ethn,
-											     fp_hash,
-											     psycopg2.Binary(id_photo)))
-	conn.commit()
-	insertions += 1
-	print "Inserted %s into the database\n"%name
+def load_patients(num_records):
+	insertions = 0
+	while insertions < num_records:
+		name = fake.name()
+		village = fake.city()
+		dob = fake.date(pattern="%Y-%m-%d")
+		gend = fake.random_element(gender)
+		ethn = fake.random_element(ethnicity)
+		fp_hash = fake.md5(raw_output=False)
+		cur.execute("INSERT INTO clinic.patients(name, village, dob, gender, ethnicity, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+												    (name,
+												     village, 
+												     dob,
+												     gend,
+												     ethn,
+												     fp_hash,
+												     psycopg2.Binary(id_photo)))
+		conn.commit()
+		insertions += 1
+		print "Inserted %s into the database\n"%name
+
+def load_nurses(num_records):
+	insertions = 0
+	while insertions < num_records:
+		name = fake.name()
+		village = fake.city()
+		dob = fake.date(pattern="%Y-%m-%d")
+		gend = fake.random_element(gender)
+		fp_hash = fake.md5(raw_output=False)
+		cur.execute("INSERT INTO clinic.nurses(name, village, dob, gender, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s)", 
+												    (name,
+												     village, 
+												     dob,
+												     gend,
+												     fp_hash,
+												     psycopg2.Binary(id_photo)))
+		conn.commit()
+		insertions += 1
+		print "Inserted %s into the database\n"%name
 # Close communication with the database
+
+load_nurses(num_records)
 cur.close()
 conn.close()
