c2b2d8ccb906ad0f77cd0cc931ff5f612c394afb
Jonas Andersson <jonas123@gmail.com>
2014-03-11 23:34:17 -0500

    created nurse class

---
+++ nurse.py
@@ -0,0 +1,9 @@
+class Nurse(object):
+	"""Nurse class. This is a terrible docstring"""
+
+	def __init__(self, name, village, dob, gender, fingerprint):
+		self.name = name
+		self.village = village
+		self.dob = dob
+		self.gender = gender
+		self.fingerprint = fingerprint
