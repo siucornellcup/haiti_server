2a459da6c6bb13ad18a44bc0908a3b05b94f520a
Jonas Andersson <Jonas123@gmail.com>
2014-03-12 19:16:51 -0500

    created db login function

--- db_bulk_load.py
+++ db_bulk_load.py
@@ -1,6 +1,11 @@
 """
 Bulk loads data into the database.
 Functions provided for bulk loading doctors, patients, and nurses tables.
+Functions provided for clearing all rows from doctors, patients and nurses tables.
+
+Lots of repetition in this script as there is no easy way to pass table and field names as parameters
+
+Don't forget to conn.close() and cur.close()
 """
 
 from faker import Factory
@@ -13,14 +18,13 @@ ethnicity = ['White','Black','American Indian',
 			 'Samoan','Other Pacific Islander','Other']
 fake = Factory.create()
 
-# Connect to an existing database
-conn = psycopg2.connect("dbname=postgres user=postgres password=hi host=localhost")
-
-# Open a cursor to perform database operations
-cur = conn.cursor()
-
 num_records = 1000
 id_photo = open("test_img.jpg","rb").read()
+
+def dblogin():
+	conn = psycopg2.connect("dbname=postgres user=postgres password=hi host=localhost")
+	cur = conn.cursor()
+	return conn, cur
 
 def load_patients(num_records):
 	insertions = 0
@@ -31,7 +35,8 @@ def load_patients(num_records):
 		gend = fake.random_element(gender)
 		ethn = fake.random_element(ethnicity)
 		fp_hash = fake.md5(raw_output=False)
-		cur.execute("INSERT INTO clinic.patients(name, village, dob, gender, ethnicity, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+		cur.execute("""INSERT INTO clinic.patients(name, village, dob, gender,
+		 ethnicity, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s, %s)""", 
 												    (name,
 												     village, 
 												     dob,
@@ -51,7 +56,8 @@ def load_nurses(num_records):
 		dob = fake.date(pattern="%Y-%m-%d")
 		gend = fake.random_element(gender)
 		fp_hash = fake.md5(raw_output=False)
-		cur.execute("INSERT INTO clinic.nurses(name, village, dob, gender, fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s)", 
+		cur.execute("""INSERT INTO clinic.nurses(name, village, dob, gender, 
+			fingerprint_hash, id_photo) VALUES (%s, %s, %s, %s, %s, %s)""", 
 												    (name,
 												     village, 
 												     dob,
@@ -61,7 +67,6 @@ def load_nurses(num_records):
 		conn.commit()
 		insertions += 1
 		print "Inserted %s into the database\n"%name
-# Close communication with the database
 
 def load_doctors(num_records):
 	insertions = 0
@@ -75,24 +80,14 @@ def load_doctors(num_records):
 		insertions += 1
 		print "Inserted %s into the database\n"%name
 
-def clear_doctors(cur,conn):
+def clear_doctors(conn,cur):
 	cur.execute("DELETE FROM clinic.doctors")
 	conn.commit()
 
-def clear_patients(cur,conn):
+def clear_patients(conn,cur):
 	cur.execute("DELETE FROM clinic.patients")
 	conn.commit()
 
-def clear_nurses(cur,conn):
+def clear_nurses(conn,cur):
 	cur.execute("DELETE FROM clinic.nurses")
 	conn.commit()
-
-# load_doctors(100)
-# load_nurses(100)
-# load_patients(100)
-clear_doctors(cur,conn)
-clear_patients(cur,conn)
-clear_nurses(cur,conn)
-
-cur.close()
-conn.close()
