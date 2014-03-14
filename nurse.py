8e0f165b060a873edee568535382884cdd940587
Jonas Andersson <Jonas123@gmail.com>
2014-03-13 20:38:51 -0500

    object attributes set by database lookup

--- nurse.py
+++ nurse.py
@@ -6,6 +6,13 @@ class Nurse(object):
 	def __init__(self, fingerprint):
 		self.fingerprint = fingerprint
 		self.fp_lookup = nurse_lookup_fp(fingerprint)
+		if self.fp_lookup != None:
+			for e in self.fp_lookup.items():
+				setattr(self, e[0], e[1])
 
-	def exists(self):
-		return self.fp_lookup != None
+	# def exists(self):
+	# 	return self.fp_lookup != None
+
+	# def set_attrs(self):
+	# 	for e in n1.fp_lookup.items():
+	# 		setattr(self, e[0], e[1])
