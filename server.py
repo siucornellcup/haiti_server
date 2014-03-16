240169ccce5826e7159dcbbcf1c623b3d3d8775d
Jonas Andersson <jonas123@gmail.com>
2014-03-15 20:54:22 -0500

    put server test code into function

--- server.py
+++ server.py
@@ -1,23 +1,28 @@
 import socket
 import json
 
-s = socket.socket()
-port = 12349
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('', port))
+def test_functionality():
+	s = socket.socket()
+	print "Creating Socket\n"
+	port = 12349
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind(('', port))
+	print "Binding socket to port",port
 
-running = True
-s. listen(5)
-while running:
-	try:
-		c, addr = s.accept()
-		print 'Got connection from', addr
-		size = c.recv(int(1024));
-		file = c.recv(int(size)+1024);
-		c.send('Connection closing...')
-		c.close()
-		new_object = json.loads(file)
-		running = False
-	except KeyboardInterrupt:
-		running = False
-s.close()
+	running = True
+	s.listen(5)
+	while running:
+		try:
+			print "Awaiting connection request...\n"
+			c, addr = s.accept()
+			print 'Got connection from', addr
+			size = c.recv(int(1024));
+			file = c.recv(int(size)+1024);
+			c.send('Connection closing...')
+			c.close()
+			new_object = json.loads(file)
+			running = False
+			return new_object
+		except KeyboardInterrupt:
+			running = False
+	s.close()
