b32f01d632c95eaffc625b83a6218d2d820c67f0
Jonas Andersson <jonas123@gmail.com>
2014-03-15 21:12:07 -0500

    created functions for sending and receiving data

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import json
+import sys
+import time
 
 def test_functionality():
 	s = socket.socket()
@@ -26,3 +28,33 @@ def test_functionality():
 		except KeyboardInterrupt:
 			running = False
 	s.close()
+
+def send_data(data, target, port):
+	size = sys.getsizeof(data)
+	sock = socket.socket()
+	sock.connect((target, port))
+	time.sleep(2)
+	sock.send(str(size))
+	sock.send(data)
+	sock.close
+	return True
+
+def receive_data(port):
+	sock = socket.socket()
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.bind(('', port))
+	sock.listen(5)
+	while True:
+		try:
+			print "Awaiting connection request...\n"
+			c, addr = sock.accept()
+			print 'Got connection from', addr
+			size = c.recv(int(1024));
+			file = c.recv(int(size)+1024);
+			c.send('Connection closing...')
+			c.close()
+			new_object = json.loads(file)
+			return new_object
+		except KeyboardInterrupt:
+			return False
+	sock.close()
