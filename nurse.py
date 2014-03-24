7e2c8e5296af0fa965fd5beae005d1b29bdf8807
Jonas Andersson <Jonas123@gmail.com>
2014-03-23 22:59:05 -0500

    created make_json and encode photo functions

--- nurse.py
+++ nurse.py
@@ -1,18 +1,41 @@
 from dbTools import nurse_lookup_fp
+import json
+import base64
 
 class Nurse(object):
 	"""Nurse class. Initialized by fingerprint md5 hash"""
 
 	def __init__(self, fingerprint):
-		self.fingerprint = fingerprint
+		self.fingerprint_hash = fingerprint
 		self.fp_lookup = nurse_lookup_fp(fingerprint)
 		if self.fp_lookup != None:
 			for e in self.fp_lookup.items():
 				setattr(self, e[0], e[1])
 			self.dob = str(self.dob)	#need to convert date object to string object so it can be serialized
-			self.id_photo = str(self.id_photo) #converts buffer to string for serialization
-			self.fp_lookup = None 
+			self.fp_lookup = None
 
+	def make_json(self):
+		"""
+		Dumps the pertinent nurse attributes into a JSON hash table
+		Returns the JSON string object
+		If the photo id has not been encoded, returns False
+		"""
+		if self.id_photo_format != 'base64':
+			return false
+		json_data = json.dumps({'name':self.name,
+								'fingerprint_hash':self.fingerprint_hash,
+								'dob':self.dob,
+								'village':self.village,
+								'gender':self.gender,
+								'id_photo':self.id_photo,
+								'id_photo_format':self.id_photo_format
+								})
+		return json_data
+
+	def encode_photo(self):
+		self.id_photo = base64.b64encode(self.id_photo)
+		self.id_photo_format = 'base64'
+		return
 	#def pack(self):
 	#	self.id_photo = str(self.id_photo)
 	# def exists(self):
